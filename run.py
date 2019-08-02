2072beb87b4cea6eae1900cd7257acdcb3d96621
ian <ian.miell@gmail.com>
2019-08-02 18:13:27 +0100

    latest

--- run.py
+++ run.py
@@ -10,7 +10,9 @@ except:
 
 s = shutit.create_session(docker_image='imiell/vultr-bare-metal', docker_rm=True, loglevel='INFO', session_type='docker')
 s.send('export TF_VAR_token=' + api_key)
+s.send('terraform init -plugin-dir /usr/local/go/bin')
 output = s.send_and_get_output('terraform apply')
+s.pause_point()
 # TODO extract IP address from output
 #ip_address =
 #s.login(command='ssh root@' + ip_address, password='vultr')
