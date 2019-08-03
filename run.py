41fd89b43174c230861a3e89adf0fc2474ef6257
ian <ian.miell@gmail.com>
2019-08-03 13:11:36 +0100

    pull

--- run.py
+++ run.py
@@ -11,8 +11,10 @@ except:
 s = shutit.create_session(loglevel='DEBUG', session_type='bash')
 s.login('docker run -ti imiell/vultr-bare-metal bash')
 s.send('export TF_VAR_token=' + api_key)
-s.pause_point()
-output = s.send_and_get_output('terraform apply')
+s.send('cd vultr-bare-metal')
+s.send('git pull')
+output = s.send_and_get_output('terraform apply -auto-approve -no-color')
+s.pause_point('')
 # TODO extract IP address from output
 #ip_address =
 #s.login(command='ssh root@' + ip_address, password='vultr')
