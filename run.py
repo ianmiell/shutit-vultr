79634fe7613283864f74e9ba0c68b46538bca6e0
ian <ian.miell@gmail.com>
2019-08-03 13:19:41 +0100

    pass region in dynamically

--- run.py
+++ run.py
@@ -10,10 +10,12 @@ except:
 
 s = shutit.create_session(loglevel='DEBUG', session_type='bash')
 s.login('docker run -ti imiell/vultr-bare-metal bash')
+# regions baremetal is available: https://www.vultr.com/api/#plans_plan_list_baremetal
+available_region = s.send_and_get_output('''curl -q https://api.vultr.com/v1/plans/list_baremetal | jq '.["100"]["available_locations"][]' | head -1''')
 s.send('export TF_VAR_token=' + api_key)
 s.send('cd vultr-bare-metal')
 s.send('git pull')
-output = s.send_and_get_output('terraform apply -auto-approve -no-color')
+output = s.send_and_get_output("terraform apply -auto-approve -no-color -var 'vultr_bm_region=" + available_region + "'")
 s.pause_point('')
 # TODO extract IP address from output
 #ip_address =
